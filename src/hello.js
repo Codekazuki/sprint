6cfa030c245c22a9d45b05c625879ba525042c0e
code kazuki <agbowurotoyin8@gmail.com>
2023-08-09 13:02:04 +0100

    update

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,13 @@ const Hello = () => {
   return (
     <>
       <h1>It will get better</h1>
+      <div>Success</div>
+      <div>Progress</div>
+      <div>Strength</div>
+      <div>Perfection</div>
+      <div></div>
+      <div>Success</div>
+      <div>Success</div>
     </>
   );
 };
