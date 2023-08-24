3f968d551119e86e3c4df1c49dcf21d1806d7b43
code kazuki <agbowurotoyin8@gmail.com>
2023-08-24 21:02:24 +0100

    useState

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,18 @@
 import "./index.css";
 import logo from "./assets/kazuki.jpg";
-import { data } from "./Data/Data.js";
+// import { data } from "./Data/Data.js";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <section className="nav">
       <img className="logo" src={logo} alt="here is it" />
-      <button>hello</button>
+      <p>you clicked {count} times</p>
+      <button onClick={handleClick}>hello</button>
     </section>
   );
 };
