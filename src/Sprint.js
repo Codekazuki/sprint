4bf784280719ade4f520a99b288c9b772bb3232b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-05 07:04:09 +0100

    scrimba update

--- src/Sprint.js
+++ src/Sprint.js
@@ -3,15 +3,19 @@ import "./index.css";
 const Sprint = () => {
   return (
     <div>
-      <h1>Fun fact about React</h1>
+      <h1 className='fact-header'>Fun fact about React</h1>
 
-      <ul>
+      <ul className='fact-items'>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
+      <ol>
+        <li>yellow</li>
+      </ol>
     </div>
   );
 };
