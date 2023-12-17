ef0f68f65af7b9376ff14117c684aff3fc4aaba2
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-17 06:05:11 +0100

    complete clean up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDom from "react-dom/client";
 
-function Starter() {
-  return <>hello world</>;
+function App() {
+  return <h1>hello react</h1>;
 }
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Starter />);
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
