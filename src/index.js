12b1afbb95025a6197dc39c1cae6dcb8d03973fe
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-18 10:34:15 +0100

    starter file added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 function App() {
-  return <h1>hello react</h1>;
+  return <h1>hello react!!!!!!!!!</h1>;
 }
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
