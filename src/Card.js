a6e7d59c645494539db864209abbd38d8f48c46f
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-22 08:25:08 +0100

    card classwork

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,37 @@
 import React from "react";
 import "./index.css";
-
+const skillData = [
+  {
+    skill: "HTML+CSS",
+    level: "expert",
+    color: "#45t398",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#45t398",
+  },
+  {
+    skill: "Javascript",
+    level: "intermediate",
+    color: "#45t398",
+  },
+  {
+    skill: "Saas",
+    level: "intermediate",
+    color: "red",
+  },
+  {
+    skill: "GIT & GitHub",
+    level: "intermediate",
+    color: "#45t398",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#45t398",
+  },
+];
 const Card = () => {
   return (
     <div className='card'>
@@ -26,23 +57,28 @@ function Bio() {
   );
 }
 function SkillList() {
+  const skill = skillData;
   return (
     <div className='skill-buttons'>
-      <Skill skill='React' emoji='💪' color='green' />
+      {/* <Skill skill='React' emoji='💪' color='green' />
       <Skill skill='Javascript' emoji='❤️' color='blue' />
       <Skill skill='HTML + CSS' emoji='💕' color='#456738' />
       <Skill skill='Tailwind' emoji='🤦‍♀️' color='#567888' />
       <Skill skill='Bootsrap' emoji='👌' color='#235699' />
-      <Skill skill='Material UI' emoji='😍' color='#845188' />
+      <Skill skill='Material UI' emoji='😍' color='#845188' /> */}
+
+      {skill.map((skill) => (
+        <Skill skillObj={skill} key={skill.skill} />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skillObj }) {
   return (
-    <button className='skill' style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <button className='skill' style={{ backgroundColor: skillObj.color }}>
+      <span>{skillObj.skill}</span>
+      <span>{skillObj.level}</span>
     </button>
   );
 }
