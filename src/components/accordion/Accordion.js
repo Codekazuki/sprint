c39ad8f3fa62dfc60dab0f8a34871314031ba38c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-20 19:44:17 +0100

    accordion ongoing

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.css";
+import { footballQuestions } from "./data";
 
 const Accordion = () => {
-  return <div className='text'>Accordion</div>;
+  const [selected, setSelected] = useState(null);
+  function handleSelection(getCurrentId) {
+    setSelected(getCurrentId);
+  }
+
+  return (
+    <section className='wrapper'>
+      <div className='accordion'>
+        {footballQuestions && footballQuestions.length > 0 ? (
+          footballQuestions.map((data) => (
+            <div>
+              <div onClick={() => handleSelection(data.id)}>
+                <h1>{data.question}</h1>
+                <span>+ </span>
+              </div>
+              {selected === data.id ? <div>{data.answer}</div> : null}
+            </div>
+          ))
+        ) : (
+          <div>There is no data at the moment</div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Accordion;
---
+++ src/components/accordion/data.js
@@ -0,0 +1,23 @@
+export const footballQuestions = [
+  {
+    id: 1,
+    question: "Who won the FIFA World Cup in 2018?",
+    answer: "France",
+  },
+  {
+    id: 2,
+    question:
+      "Which player holds the record for the most goals in a single World Cup tournament?",
+    answer: "Just Fontaine",
+  },
+  {
+    id: 3,
+    question: "Which country has won the most Copa America titles?",
+    answer: "Uruguay",
+  },
+  {
+    id: 4,
+    question: "Who is the all-time top scorer in the UEFA Champions League?",
+    answer: "Cristiano Ronaldo",
+  },
+];
